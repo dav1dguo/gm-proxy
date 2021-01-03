e241329c8db51071694dd02b020f61622b1277a3
David Guo <david.dw.guo@gmail.com>
2021-01-02 23:10:01 -0800

    initial generated version

---
+++ src/main/java/com/de/gmproxy/GmProxyApplication.java
@@ -0,0 +1,13 @@
+package com.de.gmproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/de/gmproxy/GmProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.de.gmproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
